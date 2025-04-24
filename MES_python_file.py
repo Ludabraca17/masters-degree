9d58ddc2dce50dbe060acfb9addb94e66273bb11
Ludabraca <tomislav.haring@gmail.com>
2025-04-24 14:15:20 +0200

    comment

--- MES_python_file.py
+++ MES_python_file.py
@@ -216,6 +216,7 @@ try:
 
             current_operation = sorted_operations[operation_count]
 
+            #komentar, ki bo preizkusil GitHub
             
 
             if (previousOperation_NR1["metrics"]["status"] == "Finished" and current_state_NR1 == "Idle") or (operation_count == 0):
